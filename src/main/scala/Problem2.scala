39c1efd60cb340cb8e9411705cffd13f1780c978
Kevin Kurek <kevinkurek@live.com>
2020-01-20 00:05:25 -0800

    Problem 2 readability improvement

--- src/main/scala/Problem2.scala
+++ src/main/scala/Problem2.scala
@@ -3,7 +3,7 @@
 class even_fib_calc(max_fib_num:Int) {
 
   val fibs: Stream[Int] = 0 #:: fibs.scanLeft(1)(_ + _)
-  val fibs_list_even_sum: Int = fibs.takeWhile(max_fib_num>).filter(_%2==0).sum
+  val fibs_list_even_sum: Int = fibs.takeWhile(_<max_fib_num).filter(_%2==0).sum
 
   override def toString = s"Final Sum is: ${fibs_list_even_sum}"
   }
